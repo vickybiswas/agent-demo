d78b051f62db00051245da2257996a4099ba1b1b
vickybiswas <vickybiswas@gmail.com>
2026-03-26 10:58:26 +0530

    final PR

--- frontend/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Calculator from '../components/Calculator';
-
-export default function Home() {
-  return (
-    <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh' }}>
-      <Calculator />
-    </main>
-  );
-}
--- frontend/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  sassOptions: {
-    includePaths: ['./styles'],
-  },
-};
-
-module.exports = nextConfig;
